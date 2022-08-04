6939eefad7a0b77a242c7101230f43579eb524a8
jongHyeon0000 <tamxt4047@naver.com>
2022-08-04 16:18:23 +0900

    Singleton Pattern v2

--- app/src/main/java/DesignPattern/example/singleton/EagerInitializationSingleton.java
+++ app/src/main/java/DesignPattern/example/singleton/EagerInitializationSingleton.java
@@ -1,5 +1,15 @@
 package DesignPattern.example.singleton;
 
+//장점 : 정적(static)으로 생성된 변수에 싱글톤 객체(instance : EagerInitializationSingleton)를 선언 및 할당 한다.
+//        -> 정적으로 선언된 변수는 Class Loader에 의해 클래스가 로딩될 때 Method Area(Static Area)에
+//           싱글톤 객체가 생성된다.
+//           
+//           따라서 클래스가 최초 로딩될 때 객체가 생성됨으로 Thread-safe 하다.
+//           
+//
+//단점 : 싱글톤 객체 사용 유무와 상관없이, 클래스가 로딩되는 시점에 항상 싱글톤 객체가 생성된다.
+//        -> 무거운 싱글톤 객체인 경우 메모리에 부담이 가중된다.(MemoryLeak)
+
 public class EagerInitializationSingleton {
   private static EagerInitializationSingleton instance = new EagerInitializationSingleton();
   
