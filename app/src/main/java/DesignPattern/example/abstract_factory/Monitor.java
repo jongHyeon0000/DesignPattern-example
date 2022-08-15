31562984dc9dc37de032cedf0f41426e7b3799d7
jongHyeon0000 <jongHyeon0000>
2022-08-15 22:35:21 +0900

    abstract factory infomation update

--- app/src/main/java/DesignPattern/example/abstract_factory/Monitor.java
+++ app/src/main/java/DesignPattern/example/abstract_factory/Monitor.java
@@ -1,54 +1,64 @@
 package DesignPattern.example.abstract_factory;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 import java.util.TreeMap;
 
-abstract public class Monitor{
-  protected int cost;
-  protected int weight;
-  protected int inch;
-  
-  Monitor(int cost, int weight, int inch){
-    this.cost = cost;
-    this.weight = weight;
-    this.inch = inch;
-  }
-  
-  public final static class CreateMonitorRank{
-    @FunctionalInterface
-    private interface DefaultStarCalculus<T> {
-      int getStar(T cost, T weight, T inch);
-    }
-    
-    private static final DefaultStarCalculus<Integer> defalutStarCalculus = (cost, weight, inch) -> (inch * 100000) - cost - (weight * 10000);
-    
-    @SafeVarargs // item 3
-    public static <V extends Monitor> Map<Integer, V> getRanking(DefaultStarCalculus<Integer> starCalculus, Comparator<? super Integer> comparator, V...monitors){
-      Map<Integer, V> map = new TreeMap<Integer, V>(comparator);
-      
-      for(V monitorElement : monitors) {
-        map.put(starCalculus.getStar(monitorElement.cost, monitorElement.weight, monitorElement.inch), monitorElement);
-      }
-      
-      return map;
-    }
-    
-    @SafeVarargs
-    public static <V extends Monitor> Map<Integer, V> getRanking(DefaultStarCalculus<Integer> starCalculus, V...monitors){
-      return CreateMonitorRank.getRanking(starCalculus, Integer::compare, monitors);
-    }
-    
-    @SafeVarargs
-    public static <V extends Monitor> Map<Integer, V> getRanking(Comparator<? super Integer> comparator, V...monitors) {
-      return CreateMonitorRank.getRanking(defalutStarCalculus, comparator,  monitors);
-    }
-    
-    // 가변 파라미터만 사용은 위험 -> 수정 예정
-    @SafeVarargs // item 4
-    public static <V extends Monitor> Map<Integer, V> getRanking(V...monitors) {
-      return CreateMonitorRank.getRanking(defalutStarCalculus, Integer::compare, monitors);
-    }
-  }
-}
+abstract public class Monitor {
+	protected int cost;
+	protected int weight;
+	protected int inch;
 
+	Monitor(int cost, int weight, int inch) {
+		this.cost = cost;
+		this.weight = weight;
+		this.inch = inch;
+	}
+
+	public final static class CreateMonitorRank {
+		@FunctionalInterface
+		private interface DefaultStarCalculus<T> {
+			int getStar(T cost, T weight, T inch);
+		}
+
+		private static final DefaultStarCalculus<Integer> defalutStarCalculus = (cost, weight, inch) -> (inch * 100000)
+				- cost - (weight * 10000);
+
+		@SafeVarargs // item 3
+		public static <V extends Monitor> Map<Integer, V> getRanking(DefaultStarCalculus<Integer> starCalculus,
+				Comparator<? super Integer> comparator, V... monitors) {
+			Map<Integer, V> map = new TreeMap<Integer, V>(comparator);
+
+			for (V monitorElement : monitors) {
+				map.put(starCalculus.getStar(monitorElement.cost, monitorElement.weight, monitorElement.inch), monitorElement);
+			}
+
+			return map;
+		}
+
+		@SafeVarargs
+		public static <V extends Monitor> Map<Integer, V> getRanking(DefaultStarCalculus<Integer> starCalculus,
+				V... monitors) {
+			return CreateMonitorRank.getRanking(starCalculus, Integer::compare, monitors);
+		}
+
+		@SafeVarargs
+		public static <V extends Monitor> Map<Integer, V> getRanking(Comparator<? super Integer> comparator,
+				V... monitors) {
+			return CreateMonitorRank.getRanking(defalutStarCalculus, comparator, monitors);
+		}
+
+		// 가변 파라미터만 사용은 위험 -> 수정 예정
+		@SafeVarargs // item 4
+		public static <V extends Monitor> Map<Integer, V> getRanking(V... monitors) {
+			return CreateMonitorRank.getRanking(defalutStarCalculus, Integer::compare, monitors);
+		}
+	}
+
+	public static void simplePushMethod() {
+		Object[] intArray = new Long[20]; // OK
+	}
+}
--- app/src/main/java/DesignPattern/example/abstract_factory/README.md
+++ app/src/main/java/DesignPattern/example/abstract_factory/README.md
@@ -190,7 +190,7 @@ public boolean equals(Object o){
 시작 하기 전 제네릭(Generic) 문법은 자바 기초 문법 교재나 자바 튜닝 교재 등등 수준에 상관 없이 가장 어렵고 심오한 문법으로 꼽습니다.  
 이하 내용은 아직 저에게도 어려운 내용이므로, 설명에 틀린 부분이 있더라도 양해 부탁바랍니다.  
 또한 C++의 템플릿과 자바의 제네릭은 지향점과 문법, 기술적 구현 등등 많은 부분이 상이하므로 C++의 템플릿과의 비교는 절대 안됩니다.
-경험입니다..
+
 
 **용어 정리**
 |한글 용어|영문 용어|예시|
@@ -207,4 +207,64 @@ public boolean equals(Object o){
 |제네릭 메서드|generic method|static \<E> List<E> asList(E[] a), static \<V extends Monitor> Map<Integer, V> getRanking(...)|
 |타입 토큰|type token|String.class, Keyboard.class|
 
-## 유연하고 클라이언트 친화적인 제네릭(Generic) 문법 도입
+## 제네릭 문법 도입
+
+모든 메소드를 제네릭 메소드로 만드는 것은 오히려 코드를 경직되게 만들 수 있다.  
+하지만 다양한 파라미터 타입을 받아야 하는 정적 유틸리티 클래스 라면 제네릭 문법을 적용해 제네릭 메소드로 만드는 것이 훨신 유연한 메소드가 된다.  
+  
+좋든 싫든 우리는 java 라이브러리가 제공하는 강력한 클래스들을 사용 하고 싶다면 제네릭 문법에 익숙해져야 한다.  
+간단한 예제 메서드로 살펴보자. Stack과 Object 타입 파라미터를 하나 받아 대신 push 해주는 심플한 정적 도우미 메서드다.
+
+```
+public static void simplePushMethod(Stack stack, Object obj) {
+	stack.push(obj);
+}
+```
+
+Java8 이상 부터는 컴파일은 가능하지만, 비검사 경고가 두 개 발생한다.
+
+			Stack is a raw type. References to generic type Stack<E> should be parameterized	Monitor.java  
+
+			Type safety: The method push(Object) belongs to the raw type Stack. 
+			 References to generic type Stack<E> should be parameterized	Monitor.java
+Stack은 제네릭 클래스이기 때문에, Stack 객체를 인스턴스화 할 때는 제네릭 타입으로 사용하는 것이 바람직 하다.  
+하지만 우리는 Stack을 로 타입으로 선언했다. 
+  
+로 타입으로 선언된 Stack 객체 내부의 제네릭 메소드 또한 로 타입 메소드가 된다.  
+원래라면 push 메소드의 파라미터 타입은 Stack 객체를 선언 할 때 정의 한 제네릭 타입과 같은 정규 타입 매개변수(parameter) 타입이지만,  
+우리는 Stack 객체를 로 타입으로 선언했으므로, push는 더 이상 T 타입이 아닌 Object 타입을 파라미터로 받는 구 시대 Stack을 사용하고 있다.
+  
+제네릭의 특징과 장점을 하나하나 알아보자.
+
+- **타입 안정성을 컴파일 타임 때 정의 한다**  
+
+배열과 대표적인 제네릭 타입 클래스인 List를 비교해보자.  
+배열(array)은 공변(covariant)이다. 배열과 다르게 제네릭은 불공변(invariant)이다.
+
+```
+public static void simplePushMethod() {
+	Object[] array = {10, 20}; // OK
+	List<Object> list = new ArrayList<String>(); // Type mismatch: cannot convert from ArrayList<String> to List<Object>
+}
+```
+
+array는 Object 타입을 받을 수 있는 배열이다. {10, 20} 은 Integer 타입으로 오토박싱(auto boxing) 되고,  
+Integer는 Object의 하위 타입 이므로, 문법 상 문제가 없다.
+
+배열이 유연해 보인다면, 이제 다음 예제를 살펴보자.
+
+```
+public static void simplePushMethod() {
+	Object[] objectArray = new Long[20];
+	objectArray[0] = "I'm String Type";
+}
+```
+
+예상과 달리 배열에 원소(element)를 넣으려 할 때, ArrayStoreException이 발생한다.  
+당연하게도 Long 배열에 String 타입을 넣으려 했기 때문이다.  
+objectArray
+
+정적 유틸리티 메서드는 주로 정적 도우미 클래스에 담겨져 있는 경우가 많다. Collections 클래스의 정렬(sort), 탐색(search) 메서드가 대표적이다.  
+다음 예제는 스택과 Object 타입의 파라미터 하나를 받아 스택에 push 해주는 간단한 정적 도우미 메서드다.
+
+
