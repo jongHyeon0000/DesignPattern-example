940e47f7d5f7c49d27da5ddcf00b0a9adfc0f276
jongHyeon0000 <tamxt4047@naver.com>
2022-08-24 15:17:54 +0900

    추상 팩토리 업데이트

--- app/src/main/java/DesignPattern/example/abstract_factory/monitor/AppleMonitor.java
+++ app/src/main/java/DesignPattern/example/abstract_factory/monitor/AppleMonitor.java
@@ -1,12 +1,5 @@
 package DesignPattern.example.abstract_factory.monitor;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.EmptyStackException;
-import java.util.Iterator;
-import java.util.List;
-
 public class AppleMonitor extends Monitor {
   public AppleMonitor(String name, int cost, int weight, int inch) {
     super(name, cost, weight, inch);
@@ -15,83 +8,5 @@ public class AppleMonitor extends Monitor {
   @Override
   public String toString() {
     return "I'm AppleMonitor";
-  }
-}
-
-
-class Stack<T> {
-  private int size; // 스택의 현재 사이즈
-  private Object[] elements; // 스택 내부 배열
-
-  Stack(int capacity) {
-    size = 0;
-    elements = new Object[capacity];
-  }
-
-  public void push(T t) {
-    ensureCapacity();
-    elements[++size] = t;
-  }
-
-  // 내부 배열이 가득 차면 현재 배열의 크기를 2배로 늘린다.
-  private void ensureCapacity() {
-    if (size <= 0) {
-      elements = Arrays.copyOf(elements, elements.length * 2 + 1);
-    }
-  }
-
-  public T pop() {
-    if (!isEmpty()) {
-      @SuppressWarnings("unchecked")
-      T result = (T) elements[--size];
-      elements[size] = null;
-
-      return result;
-    }
-    throw new EmptyStackException();
-  }
-
-  public void pushAll(Iterator<? extends T> src) {
-    while (src.hasNext()) {
-      push(src.next());
-    }
-  }
-
-  public boolean isEmpty() {
-    return (size <= 0) ? true : false;
-  }
-
-  public void popAll(Collection<T> dst) {
-    while (!isEmpty()) {
-      dst.add(pop());
-    }
-  }
-
-  public static void main(String[] args) {
-    Stack<Man> manStack = new Stack<>(10);
-    Collection<People> peopleList = new ArrayList<>();
-
-  }
-
-  public static <E extends Comparable<E>> E Max(List<E> list){
-    return list.stream().max((e1, e2) -> e1.compareTo(e2)).orElseThrow();
-  }
-}
-
-
-abstract class People {
-
-}
-
-
-class Man extends People implements Iterator<Man> {
-  @Override
-  public boolean hasNext() {
-    return true;
-  }
-
-  @Override
-  public Man next() {
-    return new Man();
   }
 }
